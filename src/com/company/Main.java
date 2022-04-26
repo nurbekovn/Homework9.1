e00ac82fe6ffcc72df5c90021d32b12d6d2151b1
nurbekov <nur.nurbekov.01@gmail.com>
2022-04-26 19:31:10 +0600

    Home work 9

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+//      1ден 300го чейинки сандардын арасында 3ко, 4ко жана 5ке
+//      (баарына) калдыксыз
+//      болунгон сандардын суммасын табыныз.
+public class Main {
+    public static void main(String[] args) {
+
+        int sum = 0;
+        for (int i = 0; i <=300; i++) {
+            if (i % 3 == 0 && i % 4 == 0 && i % 5 == 0) {
+                sum = sum +i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
